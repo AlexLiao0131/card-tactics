6b386073f09bdcf3930831955e544fcff19e011f
AlexLiao0131 <as315012@gmail.com>
2026-09-17 21:17:30 +0800

    Add files via upload

--- js/pack-database.js
+++ js/pack-database.js
@@ -3,7 +3,7 @@ window.CARD_PACKS={
   IMPERIAL:{id:"IMPERIAL",name:"帝國卡包",cards:["leon_card","imperial_swordsman_card","imperial_spearman_card","imperial_archer_card","imperial_heavy_guard_card","imperial_hammer_card","imperial_mage_card","imperial_cavalry_card"]},
   MAGIC:{id:"MAGIC",name:"魔法卡包",cards:["thunderstorm_card","wildfire_card","tornado_card","miracle_card","fog_card","starfall_card"]},
   ELF_EMPIRE:{id:"ELF_EMPIRE",name:"精靈帝國",cards:["reina_card","elf_shapeshifter_card","elf_ranger_card","elf_guard_card","elf_priest_card"]},
-  OPHI_SUPPLEMENT:{id:"OPHI_SUPPLEMENT",name:"奧菲補充包",cards:["ophi_card","moon_goddess_blessing_card"]},
+  OPHI_SUPPLEMENT:{id:"OPHI_SUPPLEMENT",name:"奧菲補充包",cards:["ophi_card","colin_card","moon_goddess_blessing_card"]},
   OTHER:{id:"OTHER",name:"其他／測試",cards:["rain_card","resurrection_card"]}
 };
 window.PackDatabase=(()=>({
